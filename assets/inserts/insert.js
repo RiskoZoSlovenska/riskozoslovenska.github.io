ca55d64d9ecc55957cccac5bb9909fedf6d75dd6
RiskoZoSlovenska <riskozoslovenska+git@gmail.com>
2022-02-01 14:35:07 -0500

    Minor source formatting tweaks

--- assets/inserts/insert.js
+++ assets/inserts/insert.js
@@ -1,5 +1,5 @@
 $(function () {
-	$("#footer-insert").load("/assets/inserts/footer.html")
+	$("#footer-insert" ).load("/assets/inserts/footer.html")
 	$("#sidebar-insert").load("/assets/inserts/sidebar.html")
 	$("#warning-insert").load("/assets/inserts/story-warning-overlay.html")
 });
