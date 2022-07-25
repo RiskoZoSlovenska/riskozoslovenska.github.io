a7c5f6071ab285ae1982392027b4bafc6341c359
RiskoZoSlovenska <riskozoslovenska+git@gmail.com>
2022-07-25 11:34:17 -0400

    Fixed "cheating" script only working in Firefox

--- assets/scripts/cheating.js
+++ assets/scripts/cheating.js
@@ -1,11 +1,16 @@
 /**
- * Funny meme script. Enables an overlay whenever devtools are opened. Spams
- * console, but only a little bit.
+ * Funny meme script. Enables an overlay whenever devtools are opened. Likely
+ * will break sometime soon.
+ * 
+ * Heavily adapted from https://github.com/sindresorhus/devtools-detect
+ * Permalink: https://github.com/sindresorhus/devtools-detect/blob/73719507f1ade1da63f64d3c72c487e3d2667aba/index.js
  */
 
 const OVERLAY_ID = "cheating-overlay"
 const OPEN_ATTRIB_NAME = "data-devtools-open"
 const UPDATE_INTERVAL = 250 // Milliseconds
+const THRESHOLD = 160; // idk
+
 
 function update(isOpened) {
 	let overlay = document.getElementById(OVERLAY_ID)
@@ -13,21 +18,22 @@ function update(isOpened) {
 		throw "No overlay found!"
 	}
 
+	console.log("Devtools were " + (isOpened ? "opened" : "closed"))
 	overlay.setAttribute(OPEN_ATTRIB_NAME, isOpened)
 }
 
 
-// https://stackoverflow.com/a/30638226
-let isOpened = false
-let object = /./;
-object.toString = function() {
-	isOpened = true
-	return "Devtools is open"
-};
-
+let wasOpened = false
 setInterval(() => {
-	update(isOpened)
+	let widthThreshold = globalThis.outerWidth - globalThis.innerWidth > THRESHOLD;
+	let heightThreshold = globalThis.outerHeight - globalThis.innerHeight > THRESHOLD;
 
-	isOpened = false
-	console.log(object);
+	let isOpened =
+		!(heightThreshold && widthThreshold)
+		&& (globalThis?.Firebug?.chrome?.isInitialized || widthThreshold || heightThreshold)
+
+	if (isOpened != wasOpened) {
+		update(isOpened)
+		wasOpened = isOpened
+	}
 }, UPDATE_INTERVAL)
