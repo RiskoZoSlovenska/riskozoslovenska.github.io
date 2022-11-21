f7050cea505ab16ea712cfa251c8de5291852d61
RiskoZoSlovenska <riskozoslovenska+git@gmail.com>
2022-11-21 14:57:06 -0500

    Inlined the story warning manager As it was pretty much one line used in one place, inlining couldn't hurt Also fixes the warning manager being loaded in even when not required

--- stories/warning-manager.js
+++
@@ -1,6 +0,0 @@
-function warningsAccepted() { // Exported function invoked by the button
-	document.body.removeAttribute("data-warning-accept-pending")
-	console.log("Accept button clicked")
-}
-
-console.log("Warning manager loaded successfully")
