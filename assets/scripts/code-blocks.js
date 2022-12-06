c65cf45f5ad8ba290cd9850b0b60e7259c9e4d3b
RiskoZoSlovenska <riskozoslovenska+git@gmail.com>
2022-12-06 09:27:12 -0500

    Better code block support * Added code highlighting in blocks * Specified tab width * Tweaked some inline code styles

---
+++ assets/scripts/code-blocks.js
@@ -0,0 +1,17 @@
+/*
+	Script for code block functionality, such as highlighting and (in the
+	future) line numbers and a "copy" button.
+
+	Use as <script src="/etc/code-blocks.js" type="module">
+*/
+
+// Highlighting
+let element = document.createElement("link")
+element.rel = "stylesheet"
+element.href = "/assets/styles/code-theme.css"
+document.head.appendChild(element)
+console.log("Loaded code styles")
+
+import hljs from "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@11.7.0/build/es/highlight.min.js"
+hljs.highlightAll()
+console.log("Loaded highlighter")
