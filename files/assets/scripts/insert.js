d1512274eff64634329be989a0371a48ff53e556
RiskoZoSlovenska <riskozoslovenska+git@gmail.com>
2023-11-26 23:29:41 -0500

    Fixed weird double-slash URLs in sidebar

--- files/assets/scripts/insert.js
+++ files/assets/scripts/insert.js
@@ -5,6 +5,10 @@ const ROOT = new URL("../../..", document.currentScript.src)
 const INSERTS_DIR = new URL("assets/inserts/", ROOT)
 const ROOT_PLACEHOLDER = "{{ROOT}}"
 
+if (!ROOT.pathname.endsWith("/")) { // Needed to make sure things substitute okay
+	throw new Error("ROOT path does not end with a slash!")
+}
+
 for (let element of document.querySelectorAll("*[id$='-insert']")) {
 	let template = element.id.match(/^(\w+)\-insert$/)?.[1]
 	if (!template) {
