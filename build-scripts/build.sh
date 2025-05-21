eee418204f8a88d48c5167e8465c952ea9e44f9f
RiskoZoSlovenska <riskozoslovenska+git@gmail.com>
2025-05-21 17:20:33 -0400

    Run TW only in the build directory

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -3,7 +3,7 @@ set -e
 
 src_dir=files
 build_dir=build
-css_file="$build_dir/assets/styles/style.css"
+css_file="assets/styles/style.css"
 
 if [ -e "$build_dir" ]; then
 	echo >&2 "build directory already exists!"
@@ -14,6 +14,7 @@ cp -r "$src_dir" "$build_dir"
 lua build-scripts/resolve-templates.lua
 lua build-scripts/compile-stories.lua ${1:+"$1"}
 lua build-scripts/compile-search-data.lua
+cd "$build_dir"
 tailwindcss -i "$css_file" -o "$css_file.out" --minify
 mv "$css_file.out" "$css_file"
 
